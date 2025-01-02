5652d980ac097675a857e2c1a069d0b9521ceef1
nelo <nelokwaz@gmail.com>
2025-01-02 09:52:15 +0100

    fix: refactor test using hardhat fixtures

--- hardhat-example/test/ShoeTest.js
+++ hardhat-example/test/ShoeTest.js
@@ -1,22 +1,31 @@
 const { expect } = require("chai");
 const hre = require("hardhat"); // hardhat runtime environment
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("ShoeTest", function () {
-  it("Should mint a token", async function () {
+  async function deployNFTShoeTestandMintTokenFixture() {
     const ShoeTest = await hre.ethers.getContractFactory("ShoeTest"); //This retrieves the contract factory for the ShoeTest contract, which is used to deploy instances of it.
     const [owner, otherAccount] = await ethers.getSigners(); //Retrieves an array of Ethereum accounts provided by Hardhat. These are used for interacting with the contract.
     // Pass the owner's address to the constructor
     const ShoeTestInstance = await ShoeTest.deploy(owner.address); //Deploys a new instance of the ShoeTest. The owner's address is passed to the contract's constructor.
     await ShoeTestInstance.safeMint(otherAccount.address, "FLVJ");
+    return { ShoeTestInstance };
+  }
+
+  it("Should mint a token", async function () {
+    const { ShoeTestInstance } = await loadFixture(
+      deployNFTShoeTestandMintTokenFixture
+    );
+    const [owner, otherAccount] = await ethers.getSigners(); //Retrieves an array of Ethereum accounts provided by Hardhat. These are used for interacting with the contract.
+
     expect(await ShoeTestInstance.ownerOf(0)).to.equal(otherAccount.address);
   });
 
   it("Should fail to transfer token to the wrong address", async function () {
-    const ShoeTest = await hre.ethers.getContractFactory("ShoeTest"); //This retrieves the contract factory for the ShoeTest contract, which is used to deploy instances of it.
+    const { ShoeTestInstance } = await loadFixture(
+      deployNFTShoeTestandMintTokenFixture
+    );
     const [owner, otherAccount, notTheNFTOwner] = await ethers.getSigners(); //Retrieves an array of Ethereum accounts provided by Hardhat. These are used for interacting with the contract.
-    // Pass the owner's address to the constructor
-    const ShoeTestInstance = await ShoeTest.deploy(owner.address); //Deploys a new instance of the ShoeTest. The owner's address is passed to the contract's constructor.
-    await ShoeTestInstance.safeMint(otherAccount.address, "FLVJ");
     expect(await ShoeTestInstance.ownerOf(0)).to.equal(otherAccount.address);
     await expect(
       ShoeTestInstance.connect(notTheNFTOwner).transferFrom(
